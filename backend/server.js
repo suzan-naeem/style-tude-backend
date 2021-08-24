c9596b99df32cf0c3fea2946d55866ce4eb6ddf0
[suzan naeem] <[suzannaim7@gmail.com]>
2021-08-24 07:51:16 +0200

    styletude

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import data from './data.js';
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('server is ready');
+  });
+
+app.get('/api/products/:id', (req, res) => {
+    const product = data.products.find((product) => product._id === req.params.id);
+    if (product){
+        res.send(product);
+    }else{
+        res.status(404).send({ message: 'Product not Found'});
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Serve at http://localhost:${port}`);
+}); 
